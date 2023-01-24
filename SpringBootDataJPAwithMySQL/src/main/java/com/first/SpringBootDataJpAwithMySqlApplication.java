5b0e88e5ebfc5c60c1d8205c0ecf8e4970cffb4e
vaishnavishinde2022 <120171698+vaishnavishinde2022@users.noreply.github.com>
2023-01-24 14:50:50 +0000

    Add files via upload

---
+++ SpringBootDataJPAwithMySQL/src/main/java/com/first/SpringBootDataJpAwithMySqlApplication.java
@@ -0,0 +1,15 @@
+package com.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpAwithMySqlApplication
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootDataJpAwithMySqlApplication.class, args);
+	}
+
+}
