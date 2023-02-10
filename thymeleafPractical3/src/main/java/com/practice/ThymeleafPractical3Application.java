fadb12d254c7e1cfa34a897620d7268e7afdf8e0
vaishnavishinde2022 <120171698+vaishnavishinde2022@users.noreply.github.com>
2023-02-10 13:29:42 +0000

    Add files via upload

--- thymeleafPractical3/src/main/java/com/practice/ThymeleafPractical3Application.java
+++ thymeleafPractical3/src/main/java/com/practice/ThymeleafPractical3Application.java
@@ -2,7 +2,7 @@ package com.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//@SpringBootApplication this annotation is used to mark the main class of a spring boot application
 @SpringBootApplication
 
 public class ThymeleafPractical3Application 
@@ -10,6 +10,7 @@ public class ThymeleafPractical3Application
 
 	public static void main(String[] args) 
 	{
+		//SpringApplication.run is used to bootstrap and launch a Spring application from a java main method.
 		SpringApplication.run(ThymeleafPractical3Application.class, args);
 	}
 
--- thymeleafPractical3/src/main/java/com/practice/controller/MyController.java
+++ thymeleafPractical3/src/main/java/com/practice/controller/MyController.java
@@ -1,17 +1,21 @@
 package com.practice.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
 
 @Controller
 public class MyController 
 {
 	@RequestMapping(value="/aboutRegistration",method=RequestMethod.GET)
-   public String about(Model model)
+   public String about()
    {
 	   System.out.println("Inside about handlet...");
 	   
@@ -21,13 +25,40 @@ public class MyController
 	
 	@PostMapping("/details")
 	
+	/*
+	 * Model is an interface in spring framework. it defines a placeholder for model attributes and is primarily designed for adding attribute to the model
+	 * It also use to transfer data between the view and controller of the spring MVC application.
+	 * 
+	 * @RequestParam annotation is used to extract input data that may be passed as a query,form data,or any arbitrary custom data.
+	 */
 	public String showDetails(@RequestParam("name")String name,@RequestParam("email")String email,@RequestParam("username")String username,@RequestParam("password")String password,Model model)
 	{
+		//putting data in model
 		model.addAttribute("name",name);
 		model.addAttribute("userEmail",email);
 		model.addAttribute("username",username);
 		model.addAttribute("userpassword",password);
 		
+		model.addAttribute("currentDate",new Date());
+		
 		return "about1";
+	}
+	
+	@GetMapping("/example-loop")
+	public String iterateHandler(Model model)
+	{
+		//create a ArrayList
+		
+		ArrayList<String> arraylist=new ArrayList<String>();
+		arraylist.add("Java");
+		arraylist.add("Php");
+		arraylist.add("Python");
+		arraylist.add("C");
+		arraylist.add("C++");
+		arraylist.add("Rubby");
+		
+		model.addAttribute("list",arraylist);
+				
+		return "iterate";
 	}
 }
